7bf5eae1bcad01aa9f7015227ed55767d1e7205b
priy2priya80 <144773532+priy2priya80@users.noreply.github.com>
2023-10-09 13:46:36 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,14 +1,30 @@
-#Implement a recursive function to calculate the factoriel of a given number.
+'''Implement a class called Player that represents a cricket player.The Player class should have a method called play{} which prints "The player is playing cricket.Derive two classes, Batsman and Bowler,from the Player    class.Override the play() method in each derived class to print "The batsman is batting" and "The bowler is bowling", respectively.Write a program to Create objects of both the Batsman and Bowler classes and call the play() method for each objects.''' 
 
 
-def fact(n):
-  if n ==0 or n ==1:
-    return 1
-  else:
-    return n * fact(n - 1)
-                  
-   
-number = int(input("Enter a value:"))
+#Define the base class Player 
+class Player: 
+  
+  def play(self): 
+    print("The player is playing cricket.") 
 
-res = fact(number)
-print("the factorial of {} is{}.".format(number, res))
+    
+#Define the derived class Batsman
+class Batsman(Player):
+  
+  def play(self): 
+    print("The bowler is bowling.")
+
+    
+#Define the derived class Bowler
+class Bowler(Player): 
+  
+  def play(self): 
+    print("The bowler is bowling.")
+
+    
+#Create objects of Betsman and Bowler classes 
+batsman = Batsman() 
+bowler = Bowler() 
+#Call the play() method for each object 
+batsman.play() 
+bowler.play()
